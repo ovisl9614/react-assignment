a3f8bc8353c88e8b74108fdfa374ed5a7bc6725d
ovisl9614 <ovihas12@gmail.com>
2026-09-12 20:36:29 +0600

    hero updated

---
+++ src/components/Cards.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Cards = () => {
+    return (
+        <div>
+            <div>
+                <h2>Explore the Technologies</h2>
+                <p>Pick one technology per category to build your ideal stack.</p>
+            </div>
+
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
