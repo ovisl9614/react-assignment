4113e7565ccea32341d050a204bac1216a2a446e
ovisl9614 <ovihas12@gmail.com>
2026-09-12 12:22:06 +0600

    Update hero section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,22 +6,34 @@ const Banner = () => {
     return (
         
         <div className='min-h-[400px] my-28 container mx-auto'>
-            <div className="">
-                <div>
+
+        <div className="flex items-center justify-between gap-10">
+
+        <div className='w-1/2'>
+
       <h2 className="tracking-[-1.5px] text-6xl font-extrabold text-black">
         Build Your Ideal</h2>
+
       <h2 className= "tracking-[-1.5px] text-6xl font-extrabold bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
      Development Stack</h2>
+
      <br />
-     <p className="text-3xl">Explore frontend, backend, database, and tooling options,
+     <p className="text-3xl mt-6">Explore frontend, backend, database, and tooling options,
        compare them side by side, and put together the stack that fits your
        next project.</p>
-       <button className="btn btn-lg">Large</button>
+
+       <div> <button className="btn btn-lg border-none text-white bg-linear-to-r from-[#FF5722] via-[#D81B7E] t0-[#7C3AED]">Explore Technologies</button>
+
+       <button className="btn btn-lg">Learn More</button>
+       </div>
+      
      </div>
         </div>
-        <img src="{Bannerr}" alt="" />
-        <div>
-
+        
+        <div className='w-1/2 flex justify-center'>
+        <img src={Bannerr} 
+        alt="Development Stact"
+        className='w-full max-w-[500px]' />
         </div>
         </div>
     );
